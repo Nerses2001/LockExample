1f22d02a1c57b381ecc320ba97e1b44f230107a9
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-09-29 09:32:15 -0800

    Lock impl

---
+++ CLassA.cs
@@ -0,0 +1,29 @@
+﻿
+namespace LockExample
+{
+    public class CLassA
+    {
+        private static object lockObject = new();
+
+        public static void DoWork()
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} attempting to enter the critical section.");
+
+            Monitor.Enter(lockObject);
+            try
+            {
+                Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} entered the critical section.");
+
+                Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} is performing work.");
+
+                Thread.Sleep(1000);
+            }
+            finally
+            {
+                Console.WriteLine($"Thread {Environment.CurrentManagedThreadId} leaving the critical section.");
+
+                Monitor.Exit(lockObject);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using LockExample;
+
+Thread[] threads = new Thread[5];
+for (int i = 0; i < 5; i++)
+{
+    threads[i] = new Thread(CLassA.DoWork);
+    threads[i].Start();
+}
+
+foreach (var thread in threads)
+{
+    thread.Join();
+}
+
+Console.WriteLine("Work completed.");
+Console.ReadLine();
